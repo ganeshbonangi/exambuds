76d91841ffc44ee173f2dfdc7dad4ed6c59d7115
anitha-kattamuri <kattamurianitha18@gmail.com>
2019-01-26 20:42:34 +0530

    comprehension

--- server/api/comprhension/comprehension.model.js
+++ server/api/comprhension/comprehension.model.js
@@ -3,9 +3,10 @@
 import mongoose from 'mongoose';
 
 var ComprehensionSchema = new mongoose.Schema({
+    id:Number,
     instruction:String,
     passage:String,
-    questions:{type:String,id:String}
+    questions:[{type:String,id:String}]
     });
 
 export default mongoose.model('Comprehension', ComprehensionSchema);
--- server/routes.js
+++ server/routes.js
@@ -16,6 +16,7 @@ export default function(app) {
     app.use('/api/exams', require('./api/exam'));
     app.use('/api/reports', require('./api/report'));
     app.use('/auth', require('./auth').default);
+    app.use('/api/comprhension',require('./api/comprhension'));
 
     // All undefined asset or api routes should return a 404
     app.route('/:url(api|auth|components|app|bower_components|assets)/*')
