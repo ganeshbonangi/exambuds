dcab8c1196028211d3250d553670549e2d6eb2a0
PThunug <thunuguntlapoojitha@gmail.com>
2019-01-26 08:32:22 +0530

    Reverted "-- Modified Login page"

--- client/app/account/login/login.component.ts
+++ client/app/account/login/login.component.ts
@@ -1,9 +1,8 @@
 // @flow
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 
-import {Router} from '@angular/router';
-import {AuthService} from '../../../components/auth/auth.service';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../components/auth/auth.service';
 
 interface User {
     name: string;
@@ -12,32 +11,42 @@ interface User {
 }
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    selector: 'login',
+    template: require('./login.html'),
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+    user: User = {
+        name: '',
+        email: '',
+        password: '',
+    };
+    errors = {login: undefined};
+    submitted = false;
+    AuthService;
+    Router;
 
-    loginForm: FormGroup;
 
-    constructor(private authService: AuthService) {
+    static parameters = [AuthService, Router];
+    constructor(_AuthService_: AuthService, router: Router) {
+        this.AuthService = _AuthService_;
+        this.Router = router;
+
     }
 
-    ngOnInit() {
-        this.loginForm = new FormGroup({
-            email: new FormControl('', {
-                validators: [Validators.required, Validators.email]
-            }),
-            password: new FormControl('', {validators: [Validators.required]})
-        });
-    }
+    login(form) {
+        if(form.invalid) return;
 
-    onSubmit() {
-        console.log(this.loginForm.value.email);
-        console.log(this.loginForm.value.password);
-        /*  return this.authService.login({
-              email: this.loginForm.value.email,
-              password: this.loginForm.value.password
-          })*/
+        return this.AuthService.login({
+            email: this.user.email,
+            password: this.user.password
+        })
+            .then(() => {
+                // Logged in, redirect to home
+                this.Router.navigateByUrl('/home');
+
+            })
+            .catch(err => {
+                this.errors.login = err.json().message;
+            });
     }
 }
